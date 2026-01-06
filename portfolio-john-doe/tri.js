2547ce71c8397fbfd98acc27b43d7d07467238f9
Alejo <alesilvaamir@gmail.com>
2026-01-06 14:09:07 +0100

    Challenge: Tri de noms

---
+++ portfolio-john-doe/tri.js
@@ -0,0 +1,5 @@
+let etudiants = ["Zoe", "Leo", "Ana", "Pol", "Mia", "Ian", "Eva", "Max", "Noa", "Eli"];
+etudiants.sort();
+console.log("Croissant:", etudiants);
+etudiants.reverse();
+console.log("Décroissant:", etudiants);
